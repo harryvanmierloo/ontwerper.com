1e112d53532a6e15faf7caf3f0e539dbd8f9bddb
Harry van Mierloo <info@ontwerper.com>
2018-12-08 16:55:36 +0100

    Added basic layout with theming

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,27 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+// Theme switcher
+
+const root = document.documentElement;
+const themeBtns = document.querySelectorAll(".toolbar > button");
+
+themeBtns.forEach(btn => {
+  btn.addEventListener("click", handleThemeUpdate);
+});
+
+function handleThemeUpdate(e) {
+  switch (e.target.value) {
+    case "default":
+      root.classList.remove("theme--alt");
+      root.className += " theme--default";
+      console.log("THEME = NORMAL");
+      break;
+    case "alt":
+      root.classList.remove("theme--default");
+      root.className += " theme--alt";
+      console.log("THEME = DARK");
+      break;
+  }
+}
