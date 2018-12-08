d08139ddf2c768b0ba277ed237afa550fcd43493
Harry van Mierloo <info@ontwerper.com>
2018-12-08 17:39:05 +0100

    Various color tweaks + animated theme switcher

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ if (window.netlifyIdentity) {
 // Theme switcher
 
 const root = document.documentElement;
-const themeBtns = document.querySelectorAll(".toolbar > button");
+const themeBtns = document.querySelectorAll(".theme--switcher > button");
 
 themeBtns.forEach(btn => {
   btn.addEventListener("click", handleThemeUpdate);
@@ -22,10 +22,12 @@ function handleThemeUpdate(e) {
   switch (e.target.value) {
     case "default":
       root.classList.remove("theme--alt");
+      root.classList.remove("theme--default");
       root.className += " theme--default";
       console.log("THEME = NORMAL");
       break;
     case "alt":
+      root.classList.remove("theme--alt");
       root.classList.remove("theme--default");
       root.className += " theme--alt";
       console.log("THEME = DARK");
